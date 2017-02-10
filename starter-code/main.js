fb089ed988845b65dcade6e933b2e3c6343a9754
David Brandt <davidbrandt@Davids-MacBook-Pro.local>
2017-02-10 13:39:06 -0500

    game logic popup

--- starter-code/main.js
+++ starter-code/main.js
@@ -5,3 +5,14 @@ var cardTwo="queen";
 var cardThree="king";
 var cardFour="queen";
 
+alert('Hello, friends');
+
+if (cardOne === cardThree) {
+	alert('You found a match!')
+}
+if (cardTwo === cardFour) {
+	alert('You found a match!')
+}
+else {
+	alert('Sorry, it doesn"t match!')
+}
